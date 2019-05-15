bcbcb59f62365a7164d96651b89484c6ea986630
hieupm_nara <hieupm.nara@gmail.com>
2019-05-15 13:51:50 +0700

    testing transform control

--- app.js
+++ app.js
@@ -1,31 +1,43 @@
 var WIDTH = 640,
     HEIGHT = 360;
 
-var camera, controls, scene, renderer;
-var children = [];
+var camera, controls, scene, renderer, transformControls;
+var dragItem, draggable = false;
+var startColor;
+var mouse = new THREE.Vector2();
+var collidableMesh = [];
+var movingObject;
 
 init();
 animate();
 
 function init() {
     camera = new THREE.PerspectiveCamera(70, WIDTH / HEIGHT, 1, 1000);
-    camera.position.z = 500;
-    camera.rotation.x = 1;
-    camera.rotation.y = 0.5;
-
-    // we want to control our camera
-    controls = new THREE.TrackballControls(camera);
-    controls.rotateSpeed = 2;
-    controls.zoomSpeed = 1.2;
-    controls.panSpeed = 0.8;
-    controls.noZoom = false;
-    controls.noPan = false;
-    controls.staticMoving = true;
-    controls.dynamicDampingFactor = 0.3;
+    camera.position.set(400, 400, 100);
 
     // init scene
     scene = new THREE.Scene();
     scene.background = new THREE.Color(0xf0f0f0);
+
+    // grid helper
+    var size = 400;
+    var divisions = 10;
+
+    // init floor, ceiling and walls: floor and ceiling: 400 * 400 * 600 (w * h * l)
+    var floor = new THREE.Mesh(new THREE.PlaneGeometry(400, 600), new THREE.MeshBasicMaterial({
+
+    }));
+    floor.rotation.x = Math.PI / 2;
+    collidableMesh.push(floor);
+    scene.add(floor);
+
+    var ceiling = new THREE.Mesh(new THREE.PlaneGeometry(400, 600), new THREE.MeshBasicMaterial({
+        transparent: true,
+        opacity: 0
+    }));
+    ceiling.position.set(0, 200, 0);
+    ceiling.rotation.x = Math.PI / 2;
+    scene.add(ceiling);
 
     // add light, MeshLambertMaterial cannot show color if no light
     var light = new THREE.DirectionalLight(0xffffff, 1);
@@ -33,31 +45,32 @@ function init() {
     scene.add(light);
 
     // adding cube
-    var geometry = new THREE.CubeGeometry(300, 200, 200);
+    // var geometry = new THREE.CubeGeometry(300, 200, 200);
 
-    var edges = new THREE.EdgesGeometry(geometry);
-    var line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({
-        color: 0x000000
-    }));
+    // var edges = new THREE.EdgesGeometry(geometry);
+    // var line = new THREE.LineSegments(edges, new THREE.LineBasicMaterial({
+    //     color: 0x000000
+    // }));
 
-    var material = new THREE.MeshLambertMaterial({
-        transparent: true,
-        opacity: 0
-    });
+    // var material = new THREE.MeshLambertMaterial({
+    //     transparent: true,
+    //     opacity: 0
+    // });
 
-    var mesh = new THREE.Mesh(geometry, material);
-    scene.add(mesh);
-    mesh.add(line);
+    // var mesh = new THREE.Mesh(geometry, material);
+    // mesh.position.set(0, 0, 0);
+    // scene.add(mesh);
+    // mesh.add(line);
 
-    var geometry_child = new THREE.BoxBufferGeometry(100, 100, 100);
-    var material_child = new THREE.MeshLambertMaterial({
+    var cubeGeometry = new THREE.BoxBufferGeometry(100, 100, 100);
+    var cubeMaterial = new THREE.MeshLambertMaterial({
         color: 0xba5ced,
     });
-    var mesh_child = new THREE.Mesh(geometry_child, material_child);
-    mesh_child.position.set(0, 0, 0);
+    var cube = new THREE.Mesh(cubeGeometry, cubeMaterial);
+    cube.position.set(0, 50, 0);
 
-    mesh.add(mesh_child);
-    children.push(mesh_child);
+    collidableMesh.push(cube);
+    scene.add(cube);
 
     // init renderer
     renderer = new THREE.WebGLRenderer({
@@ -68,18 +81,43 @@ function init() {
 
     document.getElementById("container").appendChild(renderer.domElement);
 
-    var dragControls = new THREE.DragControls(children, camera, renderer.domElement);
-    dragControls.addEventListener('dragstart', dragStartCallback);
-    dragControls.addEventListener('dragend', dragEndCallback);
+    // controls
+    controls = new THREE.OrbitControls(camera, renderer.domElement);
+    controls.enableDamping = true; // an animation loop is required when either damping or auto-rotation are enabled
+    controls.dampingFactor = 0.25;
+    controls.screenSpacePanning = false;
+    controls.minDistance = 100;
+    controls.maxDistance = 700;
+    controls.maxPolarAngle = Math.PI / 2;
+
+    // transform control
+    transformControls = new THREE.TransformControls(camera, renderer.domElement);
+    transformControls.addEventListener('change', render);
+    transformControls.addEventListener('dragging-changed', onDraggingChanged);
+    transformControls.attach(cube);
+    scene.add(transformControls);
+
+    var floorBoxHelper = new THREE.BoxHelper(floor, 0xff0000);
+    scene.add(floorBoxHelper);
+    var floorBox3 = new THREE.Box3();
+    floorBox3.setFromObject(floorBoxHelper);
+
+    var ceilingBoxHelper = new THREE.BoxHelper(ceiling, 0xff0000);
+    scene.add(ceilingBoxHelper);
+    var ceilingBox3 = new THREE.Box3();
+    ceilingBox3.setFromObject(ceilingBoxHelper);
+
+    var cubeBoxHelper = new THREE.BoxHelper(cube, 0xff0000);
+    scene.add(cubeBoxHelper);
+    var cubeBox3 = new THREE.Box3();
+    cubeBox3.setFromObject(cubeBoxHelper)
+    if (floorBox3.intersectsBox(cubeBox3)) {
+        console.log('intersect')
+    } else {
+        console.log('not intersect');
+    }
+
 };
-
-function dragStartCallback() {
-    controls.enabled = false;
-}
-
-function dragEndCallback() {
-    controls.enabled = true;
-}
 
 function animate() {
     requestAnimationFrame(animate);
@@ -89,4 +127,10 @@ function animate() {
 function render() {
     controls.update();
     renderer.render(scene, camera);
+}
+
+function onDraggingChanged(event) {
+    console.log('draggin')
+    controls.enabled = !event.value;
+
 }
